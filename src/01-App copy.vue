a253dad51c3b300920cc89bd3fe81f5797c051c6
baduyifei <baduyifei@qq.com>
2024-12-16 20:59:03 +0800

    change

---
+++ src/01-App copy.vue
@@ -0,0 +1,34 @@
+<!-- @format -->
+
+<template>
+	<!--  -->
+	<div id="app">
+		<div>
+			你好,{{ info }}
+			<p>v-bind:是行内属性绑定</p>
+			<p>v-on:是事件绑定</p>
+			<button v-on:click="fn">点击按钮</button>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		//
+		data() {
+			return {
+				uname: '张三',
+			};
+		},
+
+		methods: {
+			fn() {
+				//
+			},
+		},
+	};
+</script>
+
+<style>
+	/* 1 */
+</style>
--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,24 @@
 <!-- @format -->
 
 <template>
-	<!--  -->
-	<div id="app">
-		<div>
-			你好,{{ info }}
-			<p>v-bind:是行内属性绑定</p>
-			<p>v-on:是事件绑定</p>
-			<button v-on:click="fn">点击按钮</button>
-		</div>
+	<div>
+		<!--  -->
 	</div>
 </template>
 
 <script>
 	export default {
-		//
 		data() {
 			return {
-				info: '我是展示的数据',
+				uname: '张三',
 			};
 		},
 		methods: {
 			fn() {
-				console.log('页面逻辑');
-				this.info = 'aaaa';
+				//
 			},
 		},
 	};
 </script>
 
-<style>
-	/* 1 */
-</style>
+<style></style>
