242741e812f9eb4929d10b4d784e54692a337553
baduyifei <baduyifei@qq.com>
2024-12-16 20:11:25 +0800

    change

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,29 @@
 <template>
 	<!--  -->
 	<div id="app">
-		<div>你好</div>
+		<div>
+			你好,{{ info }}
+			<p>v-bind:是行内属性绑定</p>
+			<p>v-on:是事件绑定</p>
+			<button v-on:click="fn">点击按钮</button>
+		</div>
 	</div>
 </template>
 
 <script>
 	export default {
 		//
+		data() {
+			return {
+				info: '我是展示的数据',
+			};
+		},
+		methods: {
+			fn() {
+				console.log('页面逻辑');
+				this.info = 'aaaa';
+			},
+		},
 	};
 </script>
 
