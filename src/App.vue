edbf81efc9931ed73498427119da9ea93045458a
baduyifei <baduyifei@qq.com>
2024-12-16 22:12:47 +0800

    change

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,31 @@
 <!-- @format -->
 
-<template></template>
+<template>
+	<div>
+		<h1>HELLO , WORLD !</h1>
+		<h1>{{ msg }}</h1>
+		<button @click="fn">点击翻转</button>
+	</div>
+</template>
 
 <script>
-	export default {};
+	export default {
+		data() {
+			return {
+				msg: '你好张三',
+			};
+		},
+		methods: {
+			fn() {
+				const arr = this.msg.split('');
+				const newArr = arr.reverse();
+				const msg2 = newArr.join('');
+				this.msg = msg2;
+			},
+		},
+	};
 </script>
 
-<style></style>
+<style>
+	/* CSS */
+</style>
