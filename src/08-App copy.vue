ce085c76149de0b5df32e2c4b3b8e4be87f7156f
baduyifei <baduyifei@qq.com>
2024-12-18 21:22:25 +0800

    change

---
+++ src/08-App copy.vue
@@ -0,0 +1,29 @@
+<!-- @format -->
+
+<template>
+	<div>
+		<!--  -->
+		<h1>v-text和v-html</h1>
+		<p v-text="msg"></p>
+		<p v-html="msg"></p>
+	</div>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				msg: '<button>btn</button>',
+			};
+		},
+		methods: {
+			fn() {
+				//
+			},
+		},
+	};
+</script>
+
+<style>
+	/*  */
+</style>
